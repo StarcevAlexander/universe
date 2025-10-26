a45be11c94305b4f0e201942cd5a80e4013befe5
Старцев Александр <a.startsev@reo.ru>
2025-10-26 20:42:33 +0800

    email

--- backend/main.go
+++ backend/main.go
@@ -26,7 +26,7 @@ import (
 const (
 	SMTPHost     = "smtp.yandex.ru"
 	SMTPPort     = "587"
-	SMTPUsername = "79140050089@yandex.ru"
+	SMTPUsername = "alastaro@yandex.ru"
 	SMTPPassword = "79e4c955274f6f52cf51e067032d8aae"
 	ToEmail      = "79140050089@yandex.ru"
 	UploadDir    = "video"
